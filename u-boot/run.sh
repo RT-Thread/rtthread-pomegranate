655b93cdb7e98e3875781fd1f585603c26436753
BernardXiong <bernard.xiong@gmail.com>
2021-10-18 15:43:24 +0800

    Add u-boot build and run script

--- u-boot/run.sh
+++ u-boot/run.sh
@@ -2,4 +2,4 @@
 
 ROOT=$(pwd)/..
 
-${ROOT}/qemu/qemu-6.1.0/build/riscv64-softmmu/qemu-system-riscv64 -nographic -machine virt -m 256M -kernel u-boot-2021.04/u-boot.bin
+${ROOT}/qemu/qemu-6.1.0/build/riscv64-softmmu/qemu-system-riscv64 -nographic -smp 8 -machine virt -m 256M -kernel u-boot-2021.04/u-boot.bin
