a8bfc7f4f99c7657a272d1e9d69f5a2c7cdeb1e0
BernardXiong <bernard.xiong@gmail.com>
2021-10-18 15:49:13 +0800

    Add qemu URL

--- dn.sh
+++ dn.sh
@@ -1,2 +1,3 @@
 wget -c https://ftp.denx.de/pub/u-boot/u-boot-2021.04.tar.bz2
 wget -c https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.14.9.tar.xz
+wget -c https://download.qemu.org/qemu-6.1.0.tar.xz
