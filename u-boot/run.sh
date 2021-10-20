e8dd7ce0c745a2366ff6a194590b954879acae31
Bo Dong <bdong@ucrobotics.com>
2021-10-20 15:09:05 +0800

    UBOOT: Script for running uboot
    Modified script for running uboot in QEMU.

--- u-boot/run.sh
+++ u-boot/run.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
 # run u-boot in qemu
 
 ROOT=$(pwd)/..
+QEMU_RUNTIME=${ROOT}/output/qemu/bin/qemu-system-riscv64
+if [  ! -f ${QEMU_RUNTIME}]
+then
+	echo "You have to build qemu first!!!"
+	exit
+fi
 
-${ROOT}/qemu/qemu-6.1.0/build/riscv64-softmmu/qemu-system-riscv64 -nographic -smp 8 -machine virt -m 256M -kernel u-boot-2021.04/u-boot.bin
+${QEMU_RUNTIME} -nographic -smp 8 -machine virt -m 256M -kernel u-boot-2021.04/u-boot.bin
