1703a5feddda649c0f98ba8c94711a32e152b08e
Bo Dong <bdong@ucrobotics.com>
2021-10-20 18:15:16 +0800

    OPENSBI: Build script
    Build script for opensbi.

--- opensbi/build.sh
+++ opensbi/build.sh
@@ -1,4 +1,26 @@
-cd opensbi
-ROOT=$(pwd)/../..
-make CROSS_COMPILE=${ROOT}/toolchains/riscv64-unknown-elf/bin/riscv64-unknown-elf- PLATFORM=generic
-cd ..
+#!/bin/bash
+
+ROOT=$(pwd)/..
+CPU=$(cat /proc/cpuinfo| grep "processor"| wc -l)
+OUTPUT=${ROOT}/output/
+
+if [  ! -d  ${OUTPUT}    ]
+then
+        mkdir -p ${OUTPUT}
+fi
+
+if [  -d ${ROOT}/opensbi/opensbi  ]
+then
+        echo "Clean up"
+	cd ${ROOT}/opensbi/opensbi
+	git clean -xdf
+fi
+
+cd ${ROOT}/opensbi/opensbi
+git checkout v0.8
+git am ../patches/*.patch
+make CROSS_COMPILE=${ROOT}/toolchains/riscv64--glibc--bleeding-edge-2020.08-1/bin/riscv64-linux- PLATFORM=generic -j${CPU}
+
+echo "Finished build opensbi"
+cp build/platform/generic/firmware/fw_jump.bin ${OUTPUT}
+cd ${ROOT}
