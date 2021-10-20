351b5fc9f8e965fd7c4e83f094dddace769875d5
Bo Dong <bdong@ucrobotics.com>
2021-10-20 15:06:27 +0800

    UBOOT: Modify build script
    Modified build script for u-boot.

--- u-boot/build.sh
+++ u-boot/build.sh
@@ -1,5 +1,27 @@
+#!/bin/bash
+
 ROOT=$(pwd)/..
-cd u-boot-2021.04
+UBOOT="u-boot-2021.04"
+CPU=$(cat /proc/cpuinfo| grep "processor"| wc -l)
+OUTPUT=${ROOT}/output
+
+if [  ! -d  ${OUTPUT}   ]
+then
+	mkdir -p ${OUTPUT}
+fi
+
+if [  -d ${ROOT}/u-boot/${UBOOT}    ]
+then
+	echo "Clean up"
+	rm -rf ${ROOT}/u-boot/${UBOOT}
+fi
+
+tar xf ${ROOT}/tarball/${UBOOT}.tar.bz2 -C .
+cd ${UBOOT}
+for f in `ls ../patches/${UBOOT}/*.patch`; do patch -p1 < $f; done
 make qemu-riscv64_smode_defconfig
-make ARCH=riscv CROSS_COMPILE=${ROOT}/toolchains/riscv64-linux/bin/riscv64-linux-
-cd ..
+make ARCH=riscv CROSS_COMPILE=${ROOT}/toolchains/riscv64--glibc--bleeding-edge-2020.08-1/bin/riscv64-linux- -j${CPU}
+
+cp ${ROOT}/u-boot/${UBOOT}/u-boot.bin ${OUTPUT}
+echo "Finished build ${UBOOT}"
+cd ${ROOT}
