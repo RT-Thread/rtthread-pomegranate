9eb83afecfdc6346595d69c69ae4555be8e01d75
Bo Dong <bdong@ucrobotics.com>
2021-10-20 11:25:08 +0800

    Modify dn.sh

--- dn.sh
+++ dn.sh
@@ -1,3 +1,33 @@
-wget -c https://ftp.denx.de/pub/u-boot/u-boot-2021.04.tar.bz2
-wget -c https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.14.9.tar.xz
-wget -c https://download.qemu.org/qemu-6.1.0.tar.xz
+#!/bin/bash
+
+ROOT=$(pwd)
+UBOOT="u-boot-2021.04"
+LINUX="linux-5.12.3"
+QEMU="qemu-6.1.0"
+
+if [  ! -d "${ROOT}/tarball"  ]
+then
+	mkdir ${ROOT}/tarball
+else
+	rm ${ROOT}/tarball/*
+fi
+
+if [  ! -f "${ROOT}/tarball/${UBOOT}.tar.bz2"  ]
+then
+	echo "Downloading ${UBOOT}"
+	wget -P ${ROOT}/tarball -c https://ftp.denx.de/pub/u-boot/${UBOOT}.tar.bz2
+fi
+
+if [  ! -f "${ROOT}/tarball/${LINUX}.tar.xz"  ]
+then
+	echo "Downloading ${LINUX}"
+	wget -P ${ROOT}/tarball -c https://cdn.kernel.org/pub/linux/kernel/v5.x/${LINUX}.tar.xz
+fi
+
+if [  ! -f "${ROOT}/tarball/${QEMU}.tar.xz"  ]
+then
+	echo "Downloading ${QEMU}"
+	wget -P ${ROOT}/tarball -c https://download.qemu.org/${QEMU}.tar.xz
+fi
+
+echo "Finish download"
